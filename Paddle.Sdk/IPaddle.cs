b1d1f1696d476163432e752ff73ddee67f4bf118
Serhii Bevziuk <smokeelow@gmail.com>
2025-02-03 20:36:49 +0200

    - Add new APIs to IPaddle interface and update test project
    New APIs (Addresses, Customers, Prices) have been added to the IPaddle interface. Updated the test project to include a reference to the Paddle SDK for proper integration and testing.

--- Paddle.Sdk/IPaddle.cs
+++ Paddle.Sdk/IPaddle.cs
@@ -1,7 +1,16 @@
+using Paddle.Sdk.Api.Addresses;
+using Paddle.Sdk.Api.Customers;
+using Paddle.Sdk.Api.Prices;
 using Paddle.Sdk.Api.Products;
 
 namespace Paddle.Sdk;
 
 public interface IPaddle {
     IProductsApi Products { get; }
+
+    IAddressApi Addresses { get; }
+
+    ICustomersApi Customers { get; }
+
+    IPricesApi Prices { get; }
 }
