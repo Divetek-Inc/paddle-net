bca44d320a353f6e7c14e3f9da932015a50fed1c
Serhii Bevziuk <smokeelow@gmail.com>
2025-03-02 22:13:02 +0200

    Add descriptions to ExtendedTotals properties
    Descriptions were added to clarify the purpose of specific properties in the ExtendedTotals class. This enhances code readability and provides better context for developers using or maintaining the code.

--- Paddle.Sdk/Entities/Transactions/Details/ExtendedTotals.cs
+++ Paddle.Sdk/Entities/Transactions/Details/ExtendedTotals.cs
@@ -1,17 +1,19 @@
+using System.ComponentModel;
 using System.Text.Json.Serialization;
 using Paddle.Sdk.Entities.Prices;
 
 namespace Paddle.Sdk.Entities.Transactions.Details;
 
-public class ExtendedTotals : BaseTotals
-{
+public class ExtendedTotals : BaseTotals {
     [JsonPropertyName("fee")]
     public required string Fee { get; set; }
 
     [JsonPropertyName("credit")]
+    [Description("Total credit applied to this transaction. This includes credits applied using a customer's credit balance and adjustments to a billed transaction.")]
     public required string Credit { get; set; }
 
     [JsonPropertyName("balance")]
+    [Description("Total due on a transaction after credits and any payments.")]
     public required string Balance { get; set; }
 
     [JsonPropertyName("earnings")]
@@ -24,5 +26,6 @@ public class ExtendedTotals : BaseTotals
     public required CurrencyCode CurrencyCode { get; set; }
 
     [JsonPropertyName("credit_to_balance")]
+    [Description("Additional credit generated from negative details.line_items. This credit is added to the customer balance.")]
     public required string CreditToBalance { get; set; }
 }
